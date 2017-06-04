6d648cc7a24a8e2fa8881084b29bf4e52c42823a
Edwin Chen <edwinchen85@gmail.com>
2017-06-04 22:48:43 +0800

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ToDoApp } from './ToDoApp';
+import './index.css';
+
+ReactDOM.render(<ToDoApp />, document.getElementById('root'));
