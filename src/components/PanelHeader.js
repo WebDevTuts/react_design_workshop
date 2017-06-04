7b41329ff19e7df4c715aa7dab58995da333857d
Edwin Chen <edwinchen85@gmail.com>
2017-06-05 04:03:18 +0800

    Add PanelHeader component

---
+++ src/components/PanelHeader.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+export const PanelHeader = props => {
+  return (
+    <h1 className="panelHeader">
+      {props.children}
+    </h1>
+  );
+};
+
+PanelHeader.propTypes = {
+  children: PropTypes.node
+};
