1d84a5b3eb3e0ba730dac7a9fa05ab4d78d74bff
Edwin Chen <edwinchen85@gmail.com>
2017-06-04 22:32:20 +0800

    Define Panel component

---
+++ components/Panel.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+export const Panel = props => {
+  return (
+    <div className="panel">
+      {props.children}
+    </div>
+  );
+};
+
+Panel.propTypes = {
+  children: PropTypes.node
+};
