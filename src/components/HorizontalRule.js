47d3937507d321f7fb01247a939e61622b1cbb23
Edwin Chen <edwinchen85@gmail.com>
2017-06-05 21:59:19 +0800

    Add HorizontalRule component

---
+++ src/components/HorizontalRule.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+export const HorizontalRule = props => {
+  return (
+    <hr className="horizontalRule" />
+  );
+};
+
+HorizontalRule.propTypes = {
+  children: PropTypes.node
+};
