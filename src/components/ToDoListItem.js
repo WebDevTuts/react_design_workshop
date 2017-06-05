17c47de0f4036a33d5d681e53cef5a35f4f5c462
Edwin Chen <edwinchen85@gmail.com>
2017-06-05 19:55:58 +0800

    Add ToDoListItem component

---
+++ src/components/ToDoListItem.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+export const ToDoListItem = props => {
+  return (
+    <li className="toDoListItem">
+      {props.children}
+    </li>
+  );
+};
+
+ToDoListItem.propTypes = {
+  children: PropTypes.node
+};
