39b317f51d885f80ae4924838b3396727b9f6c4a
Edwin Chen <edwinchen85@gmail.com>
2017-06-06 21:44:45 +0800

    Add ToDoListItemButton component

---
+++ src/components/ToDoListItemButton.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+
+export const ToDoListItemButton = props => {
+  return (
+    <button
+      className="toDoListItem__button"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+ToDoListItemButton.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func
+};
